3f5a51b986f88fc5cc81e2d5b6a5e0afac2d30ad
Tyler Yates <tyates907@gmail.com>
2021-07-20 13:29:19 -0700

    create basic profile page

--- Screens/Profile.tsx
+++ Screens/Profile.tsx
@@ -1,11 +1,37 @@
 import React from 'react';
-import { View } from 'react-native';
-import { Title } from 'react-native-paper';
+import { View, StyleSheet } from 'react-native';
+import { Avatar, Button, Card, Divider, Headline, Paragraph, Subheading, Title } from 'react-native-paper';
 
 export default function Profile() {
     return (
         <View>
-            <Title>Profile Page</Title>
+            <Card style= {profileStyle.card}>
+                <Card.Title title="UserName" subtitle="Card Subtitle" left={() => <Avatar.Text size={48} label="UN" />} />
+                <Card.Content>
+                    <Title>About Me</Title>
+                    <Paragraph>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum cupiditate voluptate ut quae natus, tempore iste facere pariatur et corrupti doloremque iusto ducimus obcaecati officiis! Tenetur commodi officia excepturi saepe!
+                    </Paragraph>
+                    <Divider/>
+                <Card.Content>
+                    <Button style={profileStyle.button} mode="outlined">Edit</Button>
+                    <Button style={profileStyle.button} mode="contained">Delete</Button>
+                </Card.Content>
+                </Card.Content>
+            </Card>
         </View>
     );
 }
+
+const profileStyle = StyleSheet.create({
+    card: {
+        width: '90%',
+        margin: 'auto',
+        marginTop: '3em',
+        paddingVertical: '1em',
+    },
+    button: {
+        marginTop: '1em',
+       
+    }
+})
