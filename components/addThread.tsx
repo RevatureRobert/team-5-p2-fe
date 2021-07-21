00fc6411b1aac452ff10fb7863fde6379a6123d1
J19kula <j19kula@gmail.com>
2021-07-21 14:16:56 -0400

    Able to post threads

--- components/addThread.tsx
+++ components/addThread.tsx
@@ -1,26 +1,37 @@
-import React, {useState} from 'react'
+import React, {useCallback, useState} from 'react'
 import { View,Text } from 'react-native';
-import { Modal, TextInput, Portal } from 'react-native-paper';
+import { Modal, TextInput, Portal, Button } from 'react-native-paper';
+import {iThread} from './models';
+import Thread from './Thread'
 
-export default function AddThread(props) {
+
+export default function AddThread({visible, setVisible, onAdd}: {visible: any; setVisible: any; onAdd: any}) {
     const [title, setTitle] = React.useState('')
     const [description, setDescription] = React.useState('')
     const [author, setAuthor] = React.useState('')
     const [post, setPost] = React.useState('');
 
+    const addNewItemtoDB = () => {
+        onAdd({title, description, author, post})
+        setTitle('');
+        setDescription('');
+        setAuthor('');
+        setPost('');
+    } 
 
 
-    const hideModal = () => props.setVisible(false);
+    const hideModal = () => setVisible(false);
     const containerStyle = {backgroundColor: 'white', padding: 20, margin: 'auto'};
 
     return(
         <View>
             <Portal>
-                <Modal visible={props.visible} onDismiss={hideModal} contentContainerStyle={containerStyle}>
+                <Modal visible={visible} onDismiss={hideModal} contentContainerStyle={containerStyle}>
                     <TextInput label="Title" value={title} onChangeText= {text => setTitle(text)} />
                     <TextInput label="Description" value={description} onChangeText= {text => setDescription(text)} />
                     <TextInput label="Author" value={author} onChangeText= {text => setAuthor(text)} />
                     <TextInput label="Post" value={post} onChangeText= {text => setPost(text)} />
+                    <Button mode= "contained" onPress={addNewItemtoDB}>Add Thread</Button>
                 </Modal>
             </Portal>
         </View>
