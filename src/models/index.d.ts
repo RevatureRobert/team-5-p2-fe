285783a8a17b516ecb198a7ee4d89c4b8a23cb5b
Tyler Yates <tyates907@gmail.com>
2021-07-27 14:01:40 -0700

    aws configure

---
+++ src/models/index.d.ts
@@ -0,0 +1,37 @@
+import { ModelInit, MutableModel, PersistentModelConstructor } from "@aws-amplify/datastore";
+
+
+
+
+
+type UserMetaData = {
+  readOnlyFields: 'createdAt' | 'updatedAt';
+}
+
+type ThreadMetaData = {
+  readOnlyFields: 'createdAt' | 'updatedAt';
+}
+
+export declare class User {
+  readonly id: string;
+  readonly username?: string;
+  readonly password?: string;
+  readonly email?: string;
+  readonly profile?: string;
+  readonly Threads?: (Thread | null)[];
+  readonly createdAt?: string;
+  readonly updatedAt?: string;
+  constructor(init: ModelInit<User, UserMetaData>);
+  static copyOf(source: User, mutator: (draft: MutableModel<User, UserMetaData>) => MutableModel<User, UserMetaData> | void): User;
+}
+
+export declare class Thread {
+  readonly id: string;
+  readonly title?: string;
+  readonly description?: string;
+  readonly userID?: string;
+  readonly createdAt?: string;
+  readonly updatedAt?: string;
+  constructor(init: ModelInit<Thread, ThreadMetaData>);
+  static copyOf(source: Thread, mutator: (draft: MutableModel<Thread, ThreadMetaData>) => MutableModel<Thread, ThreadMetaData> | void): Thread;
+}
