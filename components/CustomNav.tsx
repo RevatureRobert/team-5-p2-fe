d50c724dd302ff29a39727b7027255bdb6a03b73
Tyler Yates <tyates907@gmail.com>
2021-07-30 07:40:29 -0700

    update Nav props

--- components/CustomNav.tsx
+++ components/CustomNav.tsx
@@ -4,16 +4,16 @@ import { Appbar } from 'react-native-paper';
 import { useSelector } from 'react-redux';
 import { IAppState } from '../redux/Store';
 
-export default function CustomNav(props) {
+export default function CustomNav({previous, navigation}) {
 
     const loggedIn = useSelector((state: IAppState) => state.userState.loggedIn)
     return(
         <View>
             <Appbar.Header>
-                {props.previous ? <Appbar.BackAction onPress={props.navigation.goBack} /> : null}
+                {previous ? <Appbar.BackAction onPress={navigation.goBack} /> : null}
                 <Appbar.Content title="Sylph"/>
-                {loggedIn ? <Appbar.Action icon="tooltip-plus" onPress={() => props.navigation.navigate('ThreadAdd')}/> : null}
-                {loggedIn ? <Appbar.Action icon="account" onPress={() => props.navigation.navigate('Profile')} /> : <Appbar.Action icon="account-plus-outline" onPress={() => props.navigation.navigate('Login')}/>}
+                {loggedIn ? <Appbar.Action icon="tooltip-plus" onPress={() => navigation.navigate('ThreadAdd')}/> : null}
+                {loggedIn ? <Appbar.Action icon="account" onPress={() => navigation.navigate('Profile')} /> : <Appbar.Action icon="account-plus-outline" onPress={() => navigation.navigate('Login')}/>}
             </Appbar.Header>
         </View>
     );
