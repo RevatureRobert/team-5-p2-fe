639e5927320db1b209bc41146f59e784f75ae6ba
J19kula <j19kula@gmail.com>
2021-08-16 11:16:29 -0400

    here yah go

--- components/Thread.tsx
+++ components/Thread.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import {iThread} from '../custom_types/object_types';
 import { Button, Card, Paragraph } from 'react-native-paper';
 import { View, StyleSheet, Image, ImageBackground } from 'react-native';
+import { color } from 'react-native-reanimated';
 // import * as style from './site.css';
 
 interface IProps {
@@ -13,14 +14,14 @@ const Threads: React.FC<IProps> = (props: IProps) =>{
         <View style= {card.view}>
             {/* <ImageBackground style={{flex: 1, width: '100%', height: '100%'}} source={require('./Forest_Background.jpg')}> */}
                 <Card style={card.thread}>
-                    <Card.Title titleNumberOfLines={10} titleStyle={{flexWrap:'wrap'}} title = {props.thread.title} subtitle= {props.thread.author}/>
+                    <Card.Title  titleNumberOfLines={10} titleStyle={{flexWrap:'wrap', color:'white'}} subtitleStyle={{color:'white'}} title = {props.thread.title} subtitle= {props.thread.author}/>
                     <Card.Content>
-                        <Paragraph>{props.thread.description}</Paragraph>
+                        <Paragraph style={{color:'white'}}>{props.thread.description}</Paragraph>
                     {/* <Card.Cover style = {images.size} source={{ uri: 'https://picsum.photos/700' }}/> */}
                     </Card.Content>
                     <Card.Actions>
-                        <Button icon="arrow-up-circle-outline"> </Button>
-                      <Button icon="arrow-down-circle-outline"> </Button>
+                        <Button color= 'white' icon="arrow-up-circle-outline"> </Button>
+                      <Button color= 'white' icon="arrow-down-circle-outline"> </Button>
                     </Card.Actions>
                 </Card>
             {/* </ImageBackground> */}
@@ -31,7 +32,7 @@ const Threads: React.FC<IProps> = (props: IProps) =>{
 
 const card = StyleSheet.create({
     view: {
-        backgroundColor: 'rgba(14, 80, 9, .85)',
+        backgroundColor: 'rgba(12, 65, 12, .85)',
     },
     thread: {
         width:'80%',
@@ -39,7 +40,7 @@ const card = StyleSheet.create({
         marginBottom: '5%',
         marginTop: '5%',
         marginLeft: '9%',
-        backgroundColor: 'rgba(40, 199, 9, 0.5)',
+        backgroundColor: 'rgba(37, 101, 37, .7)',
         alignContent: 'center',
         margin: 'auto',
     },
